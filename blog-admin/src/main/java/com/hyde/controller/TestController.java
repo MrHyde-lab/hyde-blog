3ffc185776b12841dfdc56fece38034aaf5dfce5
yuyingcong <1719317647@qq.com>
2024-06-11 16:23:14 +0800

    swagger_v3  注解使用

--- blog-admin/src/main/java/com/hyde/controller/TestController.java
+++ blog-admin/src/main/java/com/hyde/controller/TestController.java
@@ -1,17 +1,20 @@
 package com.hyde.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
-
+@Tag(name = "测试接口", description = "测试相关接口")
 @RestController
 public class TestController {
 
-    @Operation(summary = "test")
+    @Operation(
+            summary = "测试简要",
+            description = "测试详细步骤"
+    )
     @GetMapping("/test")
-    public void test(){
-        System.out.println("test");
+    public String test(){
+        return "test";
     }
 }
