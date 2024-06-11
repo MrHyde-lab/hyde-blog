57a809c54563d10701c1227c020768490897f90e
yuyingcong <1719317647@qq.com>
2024-06-11 16:30:52 +0800

    swagger_v3  注解使用

--- blog-admin/src/main/java/com/hyde/controller/TestController.java
+++ blog-admin/src/main/java/com/hyde/controller/TestController.java
@@ -5,16 +5,20 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Tag(name = "测试接口", description = "测试相关接口")
+@Tag(name = "测试controller")
 @RestController
 public class TestController {
 
-    @Operation(
-            summary = "测试简要",
-            description = "测试详细步骤"
-    )
+    @Operation(summary = "测试简要", description = "测试详细步骤")
     @GetMapping("/test")
     public String test(){
         return "test";
+    }
+
+
+    @Operation(summary = "gitbuilderTest")
+    @GetMapping("/testGitBuilder")
+    public String testGitBuilder(){
+        return "Hello GitBuilder";
     }
 }
