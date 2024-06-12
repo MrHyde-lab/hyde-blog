2cf54eb24fd0c97836e8925c0ee5a93aed4b2f1d
yuyingcong <1719317647@qq.com>
2024-06-12 16:55:20 +0800

    mybatisplus vm 模板

--- blog-admin/src/test/java/TestController.java
+++ blog-admin/src/test/java/TestController.java
@@ -1,3 +1,4 @@
+import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.generator.FastAutoGenerator;
 import com.baomidou.mybatisplus.generator.engine.FreemarkerTemplateEngine;
 import com.hyde.BlogApplication;
@@ -13,25 +14,43 @@ public class TestController {
         FastAutoGenerator.create("jdbc:mysql://localhost:3306/blog?useSSL=false&serverTimezone=UTC&useUnicode=true&characterEncoding=utf8", "root", "120507")
                 .globalConfig(builder -> builder
                         .author("hyde")
+                        .enableSwagger()
                         .outputDir(Paths.get(System.getProperty("user.dir")) + "/src/main/java")
                         .commentDate("yyyy-MM-dd")
                 )
-                .packageConfig(builder -> builder
-                        .parent("com.hyde")
-                        .entity("entity")
-                        .mapper("mapper")
-                        .service("service")
-                        .serviceImpl("service.impl")
-                        .xml("mapper.xml")
-                )
-                .strategyConfig(builder -> builder
-                        .entityBuilder()
-                        .enableLombok()
-                        .enableTableFieldAnnotation()
-                        .controllerBuilder()
-                        .enableRestStyle()
-                )
-                .templateEngine(new FreemarkerTemplateEngine())
+
+                .packageConfig(builder -> {
+                    builder.parent("com.hyde")
+                            .entity("entity")
+                            .controller("controller")
+                            .service("service")
+                            .serviceImpl("service.impl")
+                            .mapper("mapper");
+                })
+                .strategyConfig(builder -> {
+                    builder.entityBuilder()
+                            .enableLombok()
+                            .idType(IdType.ASSIGN_UUID)
+                            .formatFileName("%s")             // 格式化实体名称，%s取消首字母I
+                            .enableFileOverride()             // 覆盖已生成文件
+
+                            .controllerBuilder()
+                            .enableHyphenStyle()
+                            .enableRestStyle()                // @RestController
+                            .enableFileOverride()             // 覆盖已生成文件
+
+                            .serviceBuilder()
+                            .enableFileOverride()             // 覆盖已生成文件
+
+                            .formatServiceImplFileName("%sServiceImpl") // 格式化实体名称，%s取消首字母I
+                            .enableFileOverride()             // 覆盖已生成文件
+
+                            .mapperBuilder()
+                            .enableMapperAnnotation()         // @Mapper
+                            .formatMapperFileName("%sMapper") // 格式化实体名称，%s取消首字母I
+                            .enableFileOverride();            // 覆盖已生成文件;
+                })
+//                .templateEngine(new FreemarkerTemplateEngine())     // 使用Freemarker引擎模板，默认的是Velocity引擎模板
                 .execute();
     }
 }
