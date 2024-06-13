53bb1fee8e8cc4913d71e9d94aa89babad69ebb4
yuyingcong <1719317647@qq.com>
2024-06-13 14:31:31 +0800

    整合日志框架

---
+++ blog-framework/src/main/java/com/hyde/aspectj/LogAspect.java
@@ -0,0 +1,13 @@
+package com.hyde.aspectj;
+
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author ：yuyingcong
+ * @date ：2024/06/13   14:23:26
+ * @description： 操作日志记录
+ */
+@Slf4j
+public class LogAspect {
+}
