29b6011d089d1378d429e202372aef8cd9c94a6f
yuyingcong <1719317647@qq.com>
2024-06-14 09:53:38 +0800

    开启事务和异步任务

--- blog-admin/src/main/java/com/hyde/BlogApplication.java
+++ blog-admin/src/main/java/com/hyde/BlogApplication.java
@@ -6,12 +6,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
-
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 
 @SpringBootApplication
 @MapperScan("com.hyde.mapper")
-@EnableAspectJAutoProxy
+@EnableTransactionManagement //开启事务
+@EnableAspectJAutoProxy  //开启AOP切入
+@EnableAsync  //开启异步任务支持
 public class BlogApplication {
 
     public static void main(String[] args) {
